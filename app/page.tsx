93596d340ce53708a20e2427685eb0fb51165d21
Justice <justicelearner2021@gmail.com>
2025-10-23 11:48:33 +0000

    chore: setup project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <div className="fixed inset-5 bg-blue-500/10"></div>
+      <main className="text-black p-10">
+        <header className="w-full block text-center">
+          <h1 className="mx-auto font-bold text-3xl">HELLO ALIEN DETECTOR</h1>
+        </header>
+      </main>
+    </div>
+  );
+}
